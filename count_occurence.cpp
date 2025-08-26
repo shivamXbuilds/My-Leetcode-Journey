0d7fa6903254bf9cb4d36fca93236982d861d207
Shivam Sharma <24521@iiitu.ac.in>
2025-08-26 16:48:45 +0530

    Update count_occurence.cpp

--- count_occurence.cpp
+++ count_occurence.cpp
@@ -36,3 +36,52 @@ public:
         return ub-lb;
     }
 };
+
+
+
+
+
+
+// METHOD TWO USING ONLY BINARY SEARCH 
+class Solution {
+public:
+    int countOccurrences(const vector<int>& nums, int x) {
+        int n = nums.size();
+        int low = 0, high = n-1;
+        int first = -1;
+        int last = -1;
+        
+        while(low<=high)
+        {
+            int mid = low +(high - low) / 2;
+            if(nums[mid] == x)
+            {
+                first = mid;
+                high = mid - 1;
+            }
+            else if(nums[mid] > x)
+            {
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+
+        low=0,high = n-1;
+
+        while(low<=high)
+        {
+            int mid = low +(high - low) / 2;
+            if(nums[mid] == x)
+            {
+                last = mid;
+                low = mid + 1;
+            }
+            else if(nums[mid] > x)
+            {
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return last - first + 1;
+    }
+};
