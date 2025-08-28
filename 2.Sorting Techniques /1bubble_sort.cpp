2d6dd78d9919c747e61f8bfa9d2a7cd41bf35604
Shivam Sharma <24521@iiitu.ac.in>
2025-08-29 00:44:17 +0530

    Bubble Sort O(n^2)

---
+++ 2.Sorting Techniques /1bubble_sort.cpp
@@ -0,0 +1,20 @@
+void swap(int &x,int &y)
+{
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+void bubble_sort(vector<int> &nums)
+{
+    for(int i=0;i<nums.size() - 1;i++)
+    {
+        for(int j=0;j<nums.size() - i -1;j++)
+        {
+            if ( nums[j] > nums[j+1] )
+            {
+                swap(nums[j],nums[j+1]);
+            }
+        }
+    }
+}
