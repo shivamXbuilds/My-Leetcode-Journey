56a647f67b2802ab266470dee8b827b1822f7e0a
Shivam Sharma <24521@iiitu.ac.in>
2025-09-08 19:58:38 +0530

    Solved Prob 142 on leetcode

---
+++ Linked List/142LL_Cycle_II.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        map<ListNode*, int> mp;
+        ListNode* temp = head;
+        while(temp!=nullptr)
+        {
+            if(mp.count(temp)!=0 ) return temp;
+            else 
+            {
+                mp[temp] = 1;
+                temp = temp->next;
+            }
+        }
+        return nullptr;
+    }
+};
