583a253e3a425bc1586350e278ff6a105107fe07
Shivam Sharma <24521@iiitu.ac.in>
2025-10-10 20:29:22 +0530

    Create 45JumpGame2.cpp

---
+++ Greedy/45JumpGame2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        if(n <= 1) return 0;    
+
+        int jumps = 0;
+        int farthest = 0;
+        int currentend = 0;
+
+        for(int i=0;i<nums.size() - 1;i++)
+        {
+            farthest = max(farthest, i + nums[i]);
+
+            if(i == currentend)
+            {
+                jumps++;
+                currentend = farthest;
+            }
+        }
+
+        return jumps;
+    }
+};
