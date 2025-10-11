aac9805620ae23b4caeb703d11e91950c472d6ad
Shivam Sharma <24521@iiitu.ac.in>
2025-10-11 10:31:54 +0530

    Create 167TwoSum2.cpp

---
+++ Two Pointers/167TwoSum2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n = numbers.size();
+        int low = 0, high = n-1;
+        while(low<=high)
+        {
+            if(numbers[low] + numbers[high] == target)
+            {
+                return {low+1,high+1};
+            }
+
+            else if(numbers[low] + numbers[high] < target)
+            {
+                low++;
+            }
+            else
+            {
+                high--;
+            }
+        }
+        return{-1,-1};
+    }
+};
