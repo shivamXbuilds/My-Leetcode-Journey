27ddece1cb84ae5d1d9edafe5b4171f6182e61ec
Shivam Sharma <24521@iiitu.ac.in>
2025-10-13 13:53:00 +0530

    Create 109.cpp

---
+++ Trees/109.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        if(!head) return nullptr;
+        vector<int>extra;
+        ListNode* temp = head;
+        while(temp)
+        {
+            extra.push_back(temp->val);
+            temp = temp->next;
+        }
+
+        return Build(extra, 0, extra.size() - 1);
+    }
+
+    TreeNode* Build(vector<int>& nums, int left, int right)
+    {
+        if(left>right) return nullptr;
+        int mid = left + (right - left) / 2;
+
+        TreeNode* root = new TreeNode(nums[mid]);
+
+        root->left = Build(nums, left, mid-1);
+        root->right = Build(nums, mid+1, right);
+
+        return root;
+    }
+};
