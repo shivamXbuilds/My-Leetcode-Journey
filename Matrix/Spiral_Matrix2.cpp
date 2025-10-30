6126beed198ee17cd403c829ada3678a787d9f70
Shivam Sharma <24521@iiitu.ac.in>
2025-10-30 10:22:27 +0530

    Spiral order traversal 2

---
+++ Matrix/Spiral_Matrix2.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+       vector<vector<int>> matrix(n,vector<int>(n));
+       int top = 0, bottom = n- 1;
+       int left = 0, right = n- 1;
+       int a=1;
+       while(top <= bottom && left <= right)
+       {
+            for(int i = left; i<=right; i++)
+            {
+                matrix[top][i]=a;
+                a++;
+            }
+            top++;
+            for(int i = top;i<=bottom;i++)
+            {
+                matrix[i][right]=a;
+                a++;
+            }
+            right--;
+            if(top<=bottom)
+            {
+                for(int i = right;i>=left;i--)
+                {
+                    matrix[bottom][i]=a;
+                    a++;
+                }
+                bottom--;
+            }
+            
+            if(left<=right)
+            {
+                for(int i = bottom;i>=top;i--)
+                {
+                    matrix[i][left]=a;
+                    a++;
+                }
+                left ++;
+            }     
+       } 
+
+       return matrix;
+    }
+    
+};
